439e138db5cf5e3a4387626e17af26cffa0dec88
JuhisVain <Juho.Vainio@protonmail.ch>
2020-11-25 12:11:06 +0200

    Hex math ok

--- window.lisp
+++ window.lisp
@@ -15,6 +15,7 @@
 	    (sdl2:set-render-draw-color renderer 100 100 100 255)
 	    (sdl2:render-clear renderer)
 	    (sdl2:render-present renderer)
+	    #|
 	    (let* ((cairo-surface
 		     (cairo:create-image-surface-for-data
 		      buffer :argb32 1000 800 (* 4 1000)))
@@ -26,13 +27,30 @@
 		(cairo:set-line-width 3)
 		(cairo:move-to 0 0)
 		(cairo:line-to 1000 800)
+		;(cairo:stroke)
+		(cairo:set-line-width 2)
 		(cairo:line-to 500 200)
+		;(cairo:stroke)
+		(cairo:set-line-width 1)
 		(cairo:line-to 0 0)
-		(cairo:stroke))
+		;(cairo:stroke)
+		(cairo:set-source-rgb 1 0 0)
+		(cairo:set-line-width 1)
+		(cairo:arc 0 0 30 0 (* 2 pi))
+		(cairo:stroke)
+		)
 	      (cairo:destroy cairo-context)
-	      (cairo:destroy cairo-surface))
+	      (cairo:destroy cairo-surface))|#
 
-	    ;; https://wiki.libsdl.org/MigrationGuide
+	    (let ((test-state
+		    (make-instance 'view-state
+				   :texture texture
+				   :buffer buffer)))
+
+	    (dotimes (x 10)
+	      (dotimes (y 10)
+		(draw-hex (crd x y) test-state))))
+
 
 	    (sdl2:with-event-loop (:method :poll)
 	      
@@ -46,9 +64,66 @@
 		     )
 	      (:quit () t)
 	      )
-	    
+
+	    (sdl2:destroy-texture texture)
+
 	    ))))))
 
-;(defun render-texture (texture renderer x y)
- ; (sdl2:query-texture texture)
-  ;(sdl2:render-copy renderer texture :dest-rect (sdl2:make-rect x y ))
+(defclass view-state ()
+  ((width :initform 1000
+	  :initarg :width
+	  :accessor width)
+   (height :initform 800
+	   :initarg :height
+	   :accessor height)
+   (x-offset :initform 0
+	     :initarg :x-offset
+	     :accessor :x-offset)
+   (y-offset :initform 0
+	     :initarg :y-offset
+	     :accessor :y-offset)
+   (texture :initarg :texture
+	    :accessor texture)
+   (buffer :initarg :buffer
+	   :accessor buffer)
+   (hex-r :initform 75.0 ; ZOOM
+	  :accessor hex-r)))
+
+(defvar +sin60+ (sqrt (/ (* 3 1) 4)))
+
+(defun draw-hex (crd view-state)
+  (let* ((cairo-surface
+	   (cairo:create-image-surface-for-data
+	    (buffer view-state) :argb32
+	    (width view-state) (height view-state)
+	    (* 4 (width view-state))))
+	 (cairo-context (cairo:create-context cairo-surface)))
+
+    (let* ((r (hex-r view-state))
+	   (2r (* 2.0 r))
+	   (half-down-y (* +sin60+ r))
+	   (full-down-y (* half-down-y 2))
+	   (half-r (/ r 2.0))
+	   (three-halfs-r (* 1.5 r))
+	   (top-left-x (* (x crd) three-halfs-r))
+	   (top-left-y (+
+			(* (mod (1- (x crd)) 2) ;apply on even x
+			   half-down-y)
+			(* (y crd) (* 2.0 half-down-y)))))
+    
+      (cairo:with-context (cairo-context)
+	(if (= (x crd) (y crd) 0)
+	    (cairo:set-source-rgb 0.6 0.6 0.6) ;; (0 . 0) gray
+	    (cairo:set-source-rgb 0.6 0 0.6))
+	(cairo:set-line-width 1)
+	(cairo:move-to (+ top-left-x half-r) top-left-y) ;; NW
+	(cairo:line-to top-left-x (+ top-left-y half-down-y)) ;; W
+	(cairo:line-to (+ top-left-x half-r) (+ top-left-y full-down-y)) ;; SW
+	(cairo:line-to (+ top-left-x three-halfs-r) (+ top-left-y full-down-y)) ;; SE
+	(cairo:line-to (+ top-left-x 2r) (+ top-left-y half-down-y)) ;; E
+	(cairo:line-to (+ top-left-x three-halfs-r) top-left-y) ;; NE
+	(cairo:line-to (+ top-left-x half-r) top-left-y) ;; NW
+	(cairo:stroke)))
+    
+    (cairo:destroy cairo-context)
+    (cairo:destroy cairo-surface)))
