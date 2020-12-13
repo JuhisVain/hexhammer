7a10af9f0bb33d7ed1a2a773237ee3bb1c5681f1
JuhisVain <Juho.Vainio@protonmail.ch>
2020-12-13 12:31:47 +0200

    Unit hex funcs

--- graphics.lisp
+++ graphics.lisp
@@ -33,6 +33,26 @@
     (:SSE '(yofs :S))
     (:SE '(yofs :SW))))
 
+(defun unit-hex-crd (dir)
+  (case dir
+    (:CEN (crd 0.0 0.0))
+    (:E (crd 1.0 0.0))
+    (:NE (crd 0.75 (* +sin60+ 0.5)))
+    (:NNE (crd 0.5 +sin60+))
+    (:N (crd 0.0 +sin60+))
+    (:NNW (crd -0.5 +sin60+))
+    (:NW (crd -0.75 (* +sin60+ 0.5)))
+    (:W (crd -1.0 0.0))
+    (:SW (crd -0.75 (* +sin60+ -0.5)))
+    (:SSW (crd -0.5 (- +sin60+)))
+    (:S (crd 0.0 (- +sin60+)))
+    (:SSE (crd 0.5 (- +sin60+)))
+    (:SE (crd 0.75 (* +sin60+ -0.5)))))
+
+(defun translate-unit-crd (crd r)
+  (crd (* (x crd) r)
+       (* -1 (y crd) r)))
+
 (defun contour-offset (contour-index contour-count edge-vector-pix)
   "Returns contour's position on edge in pixels clockwise."
   (declare (fixnum contour-index contour-count)
