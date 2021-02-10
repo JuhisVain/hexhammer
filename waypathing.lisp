e051c15a1e9df13a5c2389062744a55ad8286c05
JuhisVain <Juho.Vainio@protonmail.ch>
2021-02-10 11:47:34 +0200

    Working on paths.

--- waypathing.lisp
+++ waypathing.lisp
@@ -22,11 +22,7 @@
     :accessor crd-paths-trunks)
    (tributaries
     :initarg :tributaries
-    :accessor crd-paths-tributaries)
-   (centre
-    :initarg :centre
-    :accessor crd-paths-centre
-    )))
+    :accessor crd-paths-tributaries)))
 
 (defclass crd-river () ;; Tributaries have (at least?) one point in a foreign trunk
   ((entry ;; waypoint. could be joinpoint in a delta?
@@ -57,6 +53,8 @@
    (master
     :initarg :master
     :accessor waypoint-master)
+   ;; right and left are binary trees
+   ;; where lefts are rivers/other natural features and rights are infra
    (master-left
     :initarg :master-left
     :accessor waypoint-master-left)
@@ -77,8 +75,9 @@
   (or (typep (crd-river-entry river) 'joinpoint)
       (typep (crd-river-exit river) 'joinpoint)))
 
-(defun make-crd-paths (&key trunks tributaries centre)
-  (make-instance 'crd-paths :trunks trunks :tributaries tributaries :centre centre))
+(defun make-crd-paths (&key trunks tributaries)
+  (make-instance 'crd-paths :trunks trunks :tributaries tributaries
+			    :centre (make-waypoint :vertex :cen)))
 
 (defun ensure-crd-paths (crd)
   (or (gethash crd *crd-paths*)
@@ -89,8 +88,15 @@
   (or (find-point dir/river crd-paths)
       (waypoint dir/river)))
 
-(defun make-crd-river (&key entry exit)
-  (make-instance 'crd-river :entry entry :exit exit))
+(defun make-crd-river (&key entry exit crd-paths)
+  (let ((river (make-instance 'crd-river)))
+    (when entry
+      (setf (crd-river-entry river)
+	    (ensure-waypoint entry crd-paths)))
+    (when exit
+      (setf (crd-river-exit river)
+	    (ensure-waypoint exit crd-paths)))
+    river))
 
 (defun make-waypoint (&key vertex master master-left master-right)
   (make-instance 'waypoint
@@ -101,6 +107,7 @@
 		 :trunk trunk :master master
 		 :master-left master-left :master-right master-right))
 
+(defgeneric waypoint (point))
 (defmethod waypoint ((point symbol))
   (make-waypoint :vertex point))
 
@@ -108,8 +115,6 @@
   (make-joinpoint :trunk point))
 
 (defun find-point (point crd-paths)
-  (when (eq point :CEN)
-    (return-from find-point (crd-paths-centre crd-paths)))
   (let ((defined-points
 	  (mapcan #'(lambda (crd-riv)
 		      (list (crd-river-entry crd-riv)
@@ -149,22 +154,63 @@ relative direction from FROM path when FROM path is looking towards FROM-1."
 	   :cross) ; rivers don't cross
 	  (t :left))))
 
-'(defun waypoint-add-river (waypoint river)
-  (let* ((master (waypoint-master waypoint))
-	 (master-entry-dir (get-point (crd-river-entry master)))
-	 (master-exit-dir (get-point(crd-river-exit master))))
-    ))
 
-'(defun add-river (river crd-paths)
-  (cond ((trunkp river)
-	 (crd-river-entry river))))
+;;TEST
+(defparameter *xxx* '(5 (7 NIL NIL)))
+(defun xxx (addme tree)
+  (if (<= addme (car tree))
+      (list addme tree)
+      (list (car tree)
+	    (if (cadr tree)
+		(xxx addme (cadr tree))
+		(list addme nil nil)))))
 
-'(defun def-river-paths (crd entry exit)
+;; TODO: test this
+(defun push-sub-river (river waypoint)
+  (labels ((place-in-tree (dir tree)
+	     (if (eq dir
+		     (right-or-left (crd-river-entry river)
+				    (crd-river-exit river)
+				    (crd-river-entry (car tree))
+				    (crd-river-exit (car tree))))
+		 (list river tree)
+		 (list (car tree)
+		       (if (cadr tree)
+			   (place-in-tree river (cadr tree))
+			   (list river nil nil))))))
+    
+    (let ((direction (right-or-left (crd-river-entry river)
+				    (crd-river-exit river)
+				    (crd-river-entry (waypoint-master waypoint))
+				    (crd-river-exit (waypoint-master waypoint)))))
+      (case direction
+	(:left
+	 (setf (waypoint-master-left waypoint)
+	       (place-in-tree :left (waypoint-master-left waypoint))))
+	((:right :same)
+	 (setf (waypoint-master-right waypoint)
+	       (place-in-tree :right (waypoint-master-right waypoint))))))))
+
+
+;; No idea if any of this is going to work out
+'(defun add-river-to-crd (crd entry exit)
   (let* ((crd-paths (ensure-crd-paths crd))
 	 (entry-point (ensure-waypoint entry crd-paths))
-	 (exit-point (ensure-waypoint exit crd-paths)))
-    (cond ((hex-vertexp entry exit)
-	   (make-crd-river
-	    :entry entry-point
-	    :exit exit-point)
-	   ))))
+	 (exit-point (ensure-waypoint exit crd-paths))
+	 (new-river (make-crd-river)))
+
+    ;; Initialize those with no master:
+    (if (not (waypoint-master entry-point))
+	(setf (waypoint-master entry-point) new-river)
+	(case (right-or-left entry exit
+			     (crd-river-entry (waypoint-master entry-point))
+			     (crd-river-exit (waypoint-master entry-point)))
+	  (:LEFT (setf (waypoint-master-left entry-point)))))
+
+    
+    (if (not (waypoint-master exit-point))
+	(setf (waypoint-master exit-point) new-river))
+    
+    
+    ))
+    
