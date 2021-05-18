d6aed11088ea66cd916a328f29584359c6dfdb64
JuhisVain <Juho.Vainio@protonmail.ch>
2021-05-18 10:10:39 +0300

    Looks like a working tree structure with priority accounts.

--- ht2.lisp
+++ ht2.lisp
@@ -19,20 +19,34 @@
     (setf (gethash root (prigraph-priorities graph)) (list root-node))
     graph))
 
-(defun merge-child-node (child-node children)
-  (merge 'list (list child-node) children #'< :key #'node-priority))
+(defun merge-child-node (child-node parent-node) ; but parent-node is part of child-node...?
+  ;; This isn't supposed to be ordered??
+  (setf (node-children parent-node)
+	(merge 'list (list child-node) (node-children parent-node)
+	       #'< :key #'node-priority)))
+
+(defun store-priority (node prigraph)
+  (let ((old-nodes (gethash (node-key node) (prigraph-priorities prigraph)))
+	(node-priority (node-priority node)))
+    (cond ((and (prigraph-set prigraph) ; There can be only one!
+		(< node-priority (node-priority (car old-nodes))))
+	   (let ((old-parent (node-parent (car old-nodes))))
+	     (setf (node-children old-parent)
+		   (delete (node-key node)
+			   (node-children old-parent)
+			   :key #'node-key :test #'equalp))
+	     (merge-child-node node (node-parent node))
+	     (setf (gethash (node-key node) (prigraph-priorities prigraph))
+		   (list node))))
+	  (t
+	   (merge-child-node node (node-parent node))
+	   (setf (gethash (node-key node) (prigraph-priorities prigraph))
+		 (merge 'list (list node) old-nodes
+			#'< :key #'node-priority))))))
 
 (defun add-child (child priority parent-node prigraph)
-  (let* ((priorities (prigraph-priorities prigraph))
-	 (children-nodes (gethash child priorities))
-	 (new-child-node (make-node :key child
-				    :priority priority
-				    :parent (node-key parent-node))))
-
-    (pushnew new-child-node (node-children parent-node) :test #'equalp :key #'node-key)
-    (cond ((null (prigraph-set prigraph)) ; merge child into it's position
-	   (setf (gethash child priorities)
-		 (merge-child-node new-child-node children-nodes)))
-	  ((< priority (car children-nodes)) ; replace old child with better child
-	   (setf (gethash child priorities)
-		 (list new-child-node))))))
+  (let ((new-child-node (make-node :key child
+				   :priority priority
+				   :parent parent-node)))
+    
+    (store-priority new-child-node prigraph)))
